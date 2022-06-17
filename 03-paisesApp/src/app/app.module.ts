15f90f22d1a780c3f450e6200d4114101f538075
Javier Guerrero <javier@bertonisolutions.com>
2022-06-17 15:03:01 -0500

    SPA paises app

---
+++ 03-paisesApp/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { PaisModule } from './pais/pais.module';
+import { SharedModule } from './shared/shared.module';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    PaisModule,
+    SharedModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ 03-paisesApp/src/app/pais/components/pais-input/pais-input.component.ts
@@ -0,0 +1,40 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { debounceTime, Subject } from 'rxjs';
+
+@Component({
+  selector: 'app-pais-input',
+  templateUrl: './pais-input.component.html',
+  styles: [
+    `
+      li {
+        cursor: pointer;
+      }
+    `,
+  ],
+})
+export class PaisInputComponent implements OnInit {
+  @Input() placeholder: string = '';
+  @Output() onEnter: EventEmitter<string> = new EventEmitter();
+  @Output() onDebounce: EventEmitter<string> = new EventEmitter();
+
+  debouncer: Subject<string> = new Subject();
+
+  termino: string = '';
+
+  constructor() {}
+
+  ngOnInit(): void {
+    // El debounce emite el valor despues de x tiempo después que la persona dejó de escribir
+    this.debouncer.pipe(debounceTime(300)).subscribe((valor) => {
+      this.onDebounce.emit(valor);
+    });
+  }
+
+  buscar() {
+    this.onEnter.emit(this.termino);
+  }
+
+  teclaPresionada() {
+    this.debouncer.next(this.termino);
+  }
+}
