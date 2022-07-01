310f1cb2e9b066b726d76b13e2d5c0fc625bbc8a
Javier Guerrero <javier@bertonisolutions.com>
2022-06-30 19:57:13 -0500

    custom pipes

--- 04-pipesApp/src/app/ventas/pages/ordenar/ordenar.component.ts
+++ 04-pipesApp/src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,16 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { Color, Heroe } from '../../interfaces/ventas.interface';
 
 @Component({
   selector: 'app-ordenar',
   templateUrl: './ordenar.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class OrdenarComponent implements OnInit {
+export class OrdenarComponent {
+  ordenarPor: string = '';
+  enMayusculas: boolean = true;
+  heroes: Heroe[] = [
+    {
+      nombre: 'Superman',
+      vuela: true,
+      color: Color.azul,
+    },
+    {
+      nombre: 'Batman',
+      vuela: false,
+      color: Color.negro,
+    },
+    {
+      nombre: 'Robin',
+      vuela: false,
+      color: Color.verde,
+    },
+    {
+      nombre: 'Daredevil',
+      vuela: false,
+      color: Color.rojo,
+    },
+    {
+      nombre: 'Literna Verde',
+      vuela: true,
+      color: Color.verde,
+    },
+  ];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  cambiarOrden(valor: string) {
+    this.ordenarPor = valor;
   }
-
 }
