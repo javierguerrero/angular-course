5f9f05e2e702268c90047833d81a20fa9b216f76
Javier Guerrero <javier@bertonisolutions.com>
2022-08-02 22:37:39 -0500

    heroesApp

---
+++ 05-heroesApp/src/app/heroes/pages/heroe/heroe.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Heroe } from '../interfaces/heroes.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { switchMap } from 'rxjs';
+
+@Component({
+  selector: 'app-heroe',
+  templateUrl: './heroe.component.html',
+  styles: [
+    `
+      img {
+        width: 100%;
+        border-radius: 5px;
+      }
+    `,
+  ],
+})
+export class HeroeComponent implements OnInit {
+  heroe!: Heroe;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private heroesService: HeroesService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    /*
+    this.activatedRoute.params.subscribe(({ id }) => {
+      this.heroesService.getHeroePorId(id).subscribe((resp) => {
+        this.heroe = resp;
+      });
+    });
+    */
+
+    this.activatedRoute.params
+      .pipe(switchMap(({ id }) => this.heroesService.getHeroePorId(id)))
+      .subscribe((resp) => (this.heroe = resp));
+  }
+
+  regresar() {
+    this.router.navigate(['/heroes/listado']);
+  }
+}
